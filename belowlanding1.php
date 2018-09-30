c00565d8eb28820f3e1b1eb8405eb0bda02ba440
beingyourself <crazymeshrey@gmail.com>
2018-09-30 12:02:54 +0530

    Add files via upload

---
+++ belowlanding1.php
@@ -0,0 +1,82 @@
+<!doctype html>
+<head>
+<title>Contribute page</title>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <style type="text/css">
+  
+.belowlandpage1{
+
+
+}
+.quoteandlink{
+text-align: center;
+font-family: ariel;
+font-size: 40px;
+letter-spacing: 2px;
+color:#808080;
+padding:19%;
+
+}
+.img-full{
+	align-self: center;
+}
+
+.issueimg{
+	padding-top: 10%;
+}
+</style>
+
+
+ </head>
+
+
+
+<body>
+<div class="row belowlandpage1">
+    <div class="col-sm-4 img-full">
+     <img src="photos/contribute1.jpg" alt="error loading the image" width="100%" height="750px">
+    </div>
+    <div class="col-sm-8 quoteandlink">
+     When everyone has the chance to succeed,<br>we all prosper 
+
+
+    </div>
+  </div>
+
+  <div class="row belowlandpage2">
+   
+    <div class="col-sm-7 quoteandlink">
+     When everyone has the chance to succeed,<br>we all prosper 
+    </div>
+     <div class="col-sm-5 issueimg">
+     <img src="photos/issue1.jpg" alt="error loading the image" width="100%" height="500px" >
+    </div>
+  </div>
+
+   <div class="row belowlandpage3"> 
+     <div class="col-sm-5 issueimg">
+     <img src="photos/join1.jpg" alt="error loading the image" width="100%" height="500px" >
+    </div>
+    <div class="col-sm-7 quoteandlink">
+     When everyone has the chance to succeed,<br>we all prosper 
+    </div>
+  </div>
+
+</body>
